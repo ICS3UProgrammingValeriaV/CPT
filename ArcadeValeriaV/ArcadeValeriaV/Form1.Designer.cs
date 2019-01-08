63221b29b0e1fd449c1afd7b9728d6edded1ee30
ValeriaVeverita <valeria.veverita@ocsbstudent.ca>
2019-01-08 07:00:39 -0500

    Initial commit

---
+++ ArcadeValeriaV/ArcadeValeriaV/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace ArcadeValeriaV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.tmrFirstMove = new System.Windows.Forms.Timer(this.components);
+            this.tmrSecondMove = new System.Windows.Forms.Timer(this.components);
+            this.tmrThirdMove = new System.Windows.Forms.Timer(this.components);
+            this.tmrFourthMove = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.Location = new System.Drawing.Point(303, 342);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(101, 139);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox2.Location = new System.Drawing.Point(202, 203);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(101, 139);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox3.Location = new System.Drawing.Point(101, 64);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(101, 139);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox4.Location = new System.Drawing.Point(0, -75);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(101, 139);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // tmrFirstMove
+            // 
+            this.tmrFirstMove.Tick += new System.EventHandler(this.tmrFirstMove_Tick);
+            // 
+            // tmrSecondMove
+            // 
+            this.tmrSecondMove.Tick += new System.EventHandler(this.tmrSecondMove_Tick);
+            // 
+            // tmrThirdMove
+            // 
+            this.tmrThirdMove.Tick += new System.EventHandler(this.tmrThirdMove_Tick);
+            // 
+            // tmrFourthMove
+            // 
+            this.tmrFourthMove.Tick += new System.EventHandler(this.tmrFourthMove_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 480);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Timer tmrFirstMove;
+        private System.Windows.Forms.Timer tmrSecondMove;
+        private System.Windows.Forms.Timer tmrThirdMove;
+        private System.Windows.Forms.Timer tmrFourthMove;
+    }
+}
+
---
+++ ArcadeValeriaV/ArcadeValeriaV/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArcadeValeriaV
+{
+    public partial class Form1 : Form
+    {
+        //declare global variables
+        int counter = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            if (counter == 4)
+            {
+                counter = 1;
+                pictureBox4.Location = new Point(pictureBox4.Location.X, -75);
+                tmrFourthMove.Start();
+
+            }
+            else
+            {
+                MessageBox.Show("U lost");
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (counter == 1)
+            {
+                counter++;
+                pictureBox1.Location = new Point(pictureBox1.Location.X, -75);
+                tmrFirstMove.Start();
+                
+            }
+            else
+            {
+                MessageBox.Show("U lost");
+            }
+        }
+
+        private void moveTiles(ref PictureBox pic1, ref PictureBox pic2, ref PictureBox pic3)
+        {
+            pic1.Location = new Point(pictureBox2.Location.X, pictureBox2.Location.Y + 10);
+            pic2.Location = new Point(pictureBox3.Location.X, pictureBox3.Location.Y + 10);
+            pic3.Location = new Point(pictureBox4.Location.X, pictureBox4.Location.Y + 10);
+        }
+        private void tmrFirstMove_Tick( object sender, EventArgs e)//ref PictureBox pic1, ref PictureBox pic2, ref PictureBox
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X,pictureBox1.Location.Y + 10);
+            pictureBox2.Location = new Point(pictureBox2.Location.X, pictureBox2.Location.Y + 10);
+            pictureBox3.Location = new Point(pictureBox3.Location.X, pictureBox3.Location.Y + 10);
+            pictureBox4.Location = new Point(pictureBox4.Location.X, pictureBox4.Location.Y + 10);
+            if (pictureBox1.Location.Y >=470)
+            {
+                tmrFirstMove.Stop();
+                MessageBox.Show("Ulose");
+            }
+            else if (pictureBox2.Location.Y >= 470)
+            {
+                tmrFirstMove.Stop();
+                MessageBox.Show("Ulose");
+            }
+            else if (pictureBox3.Location.Y >= 470)
+            {
+                tmrFirstMove.Stop();
+                MessageBox.Show("Ulose");
+            }
+            else if (pictureBox4.Location.Y >= 470)
+            {
+                tmrFirstMove.Stop();
+                MessageBox.Show("Ulose");
+            }
+        }
+        private void tmrThirdMove_Tick(object sender, EventArgs e)
+        {
+            
+        }
+        private void tmrFourthMove_Tick(object sender, EventArgs e)
+        {
+
+        }
+        private void tmrSecondMove_Tick(object sender, EventArgs e)
+        {
+            /*pictureBox2.Location = new Point(pictureBox2.Location.X, pictureBox2.Location.Y + 10);
+            pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + 10);
+            pictureBox3.Location = new Point(pictureBox3.Location.X, pictureBox3.Location.Y + 10);
+            pictureBox4.Location = new Point(pictureBox4.Location.X, pictureBox4.Location.Y + 10);
+            
+            if (pictureBox3.Location.Y >= 342)
+            {
+                Console.WriteLine("Stop");
+                tmrSecondMove.Stop();
+            }
+            else
+            {
+               if (pictureBox2.Location.Y >= 470)
+               {
+                    tmrSecondMove.Stop();
+                    MessageBox.Show("U lost");
+               }
+               else
+                {
+
+                }
+            
+            else if (pictureBox2.Location.Y != -75)
+            {
+                tmrSecondMove.Stop();
+                MessageBox.Show("U lost");
+            }*/
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            if (counter == 2)
+            {
+                counter++;
+                pictureBox2.Location = new Point(pictureBox2.Location.X, -75);
+                tmrFirstMove.Start();
+                //tmrFirstMove.Start(ref pictureBox1, ref pictureBox3, ref pictureBox4);
+
+            }
+            else
+            {
+                MessageBox.Show("U lost");
+            }
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            if (counter == 3)
+            {
+                counter++;
+                pictureBox3.Location = new Point(pictureBox3.Location.X, -75);
+                tmrFirstMove.Start();
+                //tmrFirstMove.Start(ref pictureBox1, ref pictureBox3, ref pictureBox4);
+
+            }
+            else
+            {
+                MessageBox.Show("U lost");
+            }
+        }
+
+       
+    }
+}
